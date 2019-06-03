scipopt/scip

commit 561af4213546b080acabe9fb5818efc70763f0f6

always like bound changes to 0 (issue #40)
- a new lower bound is better also if oldlb < 0 and newlb >= 0
- a new upper bound is better also if oldub > 0 and newub <= 0

src/scip/set.c
@@ -5977,7 +5977,8 @@ SCIP_Real SCIPsetDualfeasFrac(
 }
 
 /** checks, if the given new lower bound is at least min(oldub - oldlb, |oldlb|) times the bound
- *  strengthening epsilon better than the old one
+ *  strengthening epsilon better than the old one or the change in the lower bound would fix the
+ *  sign of the variable
  */
 SCIP_Bool SCIPsetIsLbBetter(
    SCIP_SET*             set,                /**< global SCIP settings */
@@ -5991,13 +5992,18 @@ SCIP_Bool SCIPsetIsLbBetter(
    assert(set != NULL);
    assert(SCIPsetIsLE(set, oldlb, oldub));
 
+   /* if lower bound is moved to 0 or higher, always accept bound change */
+   if( oldlb < 0.0 && newlb >= 0.0 )
+      return TRUE;
+
    eps = REALABS(oldlb);
    eps = MIN(oldub - oldlb, eps);
    return EPSGT(newlb, oldlb, set->num_boundstreps * MAX(eps, 1e-3));
 }
 
 /** checks, if the given new upper bound is at least min(oldub - oldlb, |oldub|) times the bound
- *  strengthening epsilon better than the old one
+ *  strengthening epsilon better than the old one or the change in the upper bound would fix the
+ *  sign of the variable
  */
 SCIP_Bool SCIPsetIsUbBetter(
    SCIP_SET*             set,                /**< global SCIP settings */
@@ -6010,6 +6016,10 @@ SCIP_Bool SCIPsetIsUbBetter(
 
    assert(set != NULL);
    assert(SCIPsetIsLE(set, oldlb, oldub));
+
+   /* if upper bound is moved to 0 or lower, always accept bound change */
+   if( oldub > 0.0 && newub <= 0.0 )
+      return TRUE;
 
    eps = REALABS(oldub);
    eps = MIN(oldub - oldlb, eps);

src/scip/set.h
@@ -1582,7 +1582,10 @@ SCIP_Real SCIPsetDualfeasFrac(
    SCIP_Real             val                 /**< value to return fractional part for */
    );
 
-/** checks, if the given new lower bound is tighter (w.r.t. bound strengthening epsilon) than the old one */
+/** checks, if the given new lower bound is at least min(oldub - oldlb, |oldlb|) times the bound
+ *  strengthening epsilon better than the old one or the change in the lower bound would fix the
+ *  sign of the variable
+ */
 extern
 SCIP_Bool SCIPsetIsLbBetter(
    SCIP_SET*             set,                /**< global SCIP settings */
@@ -1591,7 +1594,10 @@ SCIP_Bool SCIPsetIsLbBetter(
    SCIP_Real             oldub               /**< old upper bound */
    );
 
-/** checks, if the given new upper bound is tighter (w.r.t. bound strengthening epsilon) than the old one */
+/** checks, if the given new upper bound is at least min(oldub - oldlb, |oldub|) times the bound
+ *  strengthening epsilon better than the old one or the change in the upper bound would fix the
+ *  sign of the variable
+ */
 extern
 SCIP_Bool SCIPsetIsUbBetter(
    SCIP_SET*             set,                /**< global SCIP settings */

src/scip/var.c
@@ -860,7 +860,7 @@ SCIP_RETCODE boundchgApplyGlobal(
    newbound = SCIPboundchgGetNewbound(boundchg);
    boundtype = SCIPboundchgGetBoundtype(boundchg);
 
-   /* check if the bound change is redundant which can happen due to a (better) global bound change which was perforemed
+   /* check if the bound change is redundant which can happen due to a (better) global bound change which was performed
     * after that bound change was applied
     *
     * @note a global bound change is not captured by the redundant member of the bound change data structure

microhuang/scipoptsuite

commit 3e18623c83f3a8ed921f791695928a5cf2014e8c

add missing change-bound-to-zero check to SCIPisXbBetter when NDEBUG def.
- fixes different behavior on instance oil in #2650
- fixes bug introduced with 561af421354
- also make sure that non-NDEBUG variant behaves exactly the same (i.e.,
  also regarding roundoff errors) as NDEBUG version by using the same code

src/scip/set.c
@@ -6704,8 +6704,6 @@ SCIP_Bool SCIPsetIsLbBetter(
    SCIP_Real             oldub               /**< old upper bound */
    )
 {
-   SCIP_Real eps;
-
    assert(set != NULL);
    assert(SCIPsetIsLE(set, oldlb, oldub));
 
@@ -6713,9 +6711,7 @@ SCIP_Bool SCIPsetIsLbBetter(
    if( oldlb < 0.0 && newlb >= 0.0 )
       return TRUE;
 
-   eps = REALABS(oldlb);
-   eps = MIN(oldub - oldlb, eps);
-   return EPSGT(newlb, oldlb, set->num_boundstreps * MAX(eps, 1e-3));
+   return EPSGT(newlb, oldlb, set->num_boundstreps * MAX(MIN(oldub - oldlb, REALABS(oldlb)), 1e-3));  /*lint !e666*/
 }
 
 /** checks, if the given new upper bound is at least min(oldub - oldlb, |oldub|) times the bound
@@ -6729,8 +6725,6 @@ SCIP_Bool SCIPsetIsUbBetter(
    SCIP_Real             oldub               /**< old upper bound */
    )
 {
-   SCIP_Real eps;
-
    assert(set != NULL);
    assert(SCIPsetIsLE(set, oldlb, oldub));
 
@@ -6738,9 +6732,7 @@ SCIP_Bool SCIPsetIsUbBetter(
    if( oldub > 0.0 && newub <= 0.0 )
       return TRUE;
 
-   eps = REALABS(oldub);
-   eps = MIN(oldub - oldlb, eps);
-   return EPSLT(newub, oldub, set->num_boundstreps * MAX(eps, 1e-3));
+   return EPSLT(newub, oldub, set->num_boundstreps * MAX(MIN(oldub - oldlb, REALABS(oldub)), 1e-3));  /*lint !e666*/
 }
 
 /** checks, if the given cut's efficacy is larger than the minimal cut efficacy */

src/scip/set.h
@@ -1655,9 +1655,9 @@ SCIP_Bool SCIPsetIsSumRelGE(
 #define SCIPsetDualfeasRound(set, val)         ( EPSROUND(val, (set)->num_dualfeastol) )
 #define SCIPsetDualfeasFrac(set, val)          ( EPSFRAC(val, (set)->num_dualfeastol) )
 
-#define SCIPsetIsLbBetter(set, newlb, oldlb, oldub) ( EPSGT(newlb, oldlb, \
+#define SCIPsetIsLbBetter(set, newlb, oldlb, oldub) ( ((oldlb) < 0.0 && (newlb) >= 0.0) || EPSGT(newlb, oldlb, \
          set->num_boundstreps * MAX(MIN((oldub) - (oldlb), REALABS(oldlb)), 1e-3)) )
-#define SCIPsetIsUbBetter(set, newub, oldlb, oldub) ( EPSLT(newub, oldub, \
+#define SCIPsetIsUbBetter(set, newub, oldlb, oldub) ( ((oldub) > 0.0 && (newub) <= 0.0) || EPSLT(newub, oldub, \
          set->num_boundstreps * MAX(MIN((oldub) - (oldlb), REALABS(oldub)), 1e-3)) )
 #define SCIPsetIsEfficacious(set, root, efficacy) \
    ( root ? EPSP(efficacy, (set)->sepa_minefficacyroot) : EPSP(efficacy, (set)->sepa_minefficacy) )